45d46cf000ca1cd8b935dc56a0b28d48e06b2605
venutti <lucasvenutti23@gmail.com>
2024-03-12 17:19:00 -0300

    Add new transaction type

--- src/modules/stellar/domain/stellar-transaction.domain.ts
+++ src/modules/stellar/domain/stellar-transaction.domain.ts
@@ -1,10 +1,12 @@
 import { Base } from '@/common/domain/base.domain';
 
 export enum TRANSACTION_TYPE {
+  CREATE = 'create',
   CONFIRM = 'confirm',
   CONSOLIDATE = 'consolidate',
   DELIVER = 'deliver',
 }
+
 export class StellarTransaction extends Base {
   orderId: number;
   type: TRANSACTION_TYPE;
