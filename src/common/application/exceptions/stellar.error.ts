d75899fc54846ca112bce9f21d9c16fec4fcc710
venutti <lucasvenutti23@gmail.com>
2024-03-13 11:13:31 -0300

    Refactor Stellar service to create executeTransaction

--- src/common/application/exceptions/stellar.error.ts
+++ src/common/application/exceptions/stellar.error.ts
@@ -1,13 +1,12 @@
 export enum ERROR_CODES {
   CONFIG_ISSUER_ACCOUNT_ERROR = 'CONFIG_ISSUER_ACCOUNT_ERROR',
-  CREATE_CORE_ACCOUNTS_ERROR = 'CREATE_CORE_ACCOUNTS_ERROR',
-  CREATE_ASSETS_ERROR = 'CREATE_ASSET_ERROR',
   CREATE_ORDER_ERROR = 'CREATE_ORDER_ERROR',
   CONFIRM_ORDER_ERROR = 'CONFIRM_ORDER_ERROR',
   CONSOLIDATE_ORDER_ERROR = 'CONSOLIDATE_ORDER_ERROR',
   DELIVER_ORDER_ERROR = 'DELIVER_ORDER_ERROR',
   GENERIC_ERROR = 'GENERIC_ERROR',
-  ORDER_ALREADY_CONFIRMED_ERROR = 'ORDER_ALREADY_CONFIRMED_ERROR',
+  ORDER_UNABLE_TO_CREATE_ERROR = 'ORDER_UNABLE_TO_CREATE_ERROR',
+  ORDER_UNABLE_TO_CONFIRM_ERROR = 'ORDER_UNABLE_TO_CONFIRM_ERROR',
   ORDER_UNABLE_TO_CONSOLIDATE_ERROR = 'ORDER_UNABLE_TO_CONSOLIDATE_ERROR',
   ORDER_UNABLE_TO_DELIVER_ERROR = 'ORDER_UNABLE_TO_DELIVER_ERROR',
 }
@@ -15,9 +14,6 @@ export enum ERROR_CODES {
 export const ERROR_MESSAGES: { [key in ERROR_CODES]: string } = {
   [ERROR_CODES.CONFIG_ISSUER_ACCOUNT_ERROR]:
     'An error occurred while configuring issuer account',
-  [ERROR_CODES.CREATE_CORE_ACCOUNTS_ERROR]:
-    'An error occurred while creating core accounts',
-  [ERROR_CODES.CREATE_ASSETS_ERROR]: 'An error occurred while creating assets',
   [ERROR_CODES.CREATE_ORDER_ERROR]: 'An error occurred while creating order',
   [ERROR_CODES.CONFIRM_ORDER_ERROR]:
     'An error occurred while confirming order transaction',
@@ -26,8 +22,8 @@ export const ERROR_MESSAGES: { [key in ERROR_CODES]: string } = {
   [ERROR_CODES.DELIVER_ORDER_ERROR]:
     'An error occurred while delivering order transaction',
   [ERROR_CODES.GENERIC_ERROR]: 'An error occurred in the Stellar service',
-  [ERROR_CODES.ORDER_ALREADY_CONFIRMED_ERROR]:
-    'The order has already been confirmed',
+  [ERROR_CODES.ORDER_UNABLE_TO_CREATE_ERROR]: 'The order cannot be created',
+  [ERROR_CODES.ORDER_UNABLE_TO_CONFIRM_ERROR]: 'The order cannot be confirmed',
   [ERROR_CODES.ORDER_UNABLE_TO_CONSOLIDATE_ERROR]:
     'The order cannot be consolidated',
   [ERROR_CODES.ORDER_UNABLE_TO_DELIVER_ERROR]: 'The order cannot be delivered',
--- src/modules/stellar/application/services/stellar.service.ts
+++ src/modules/stellar/application/services/stellar.service.ts
@@ -5,12 +5,11 @@ import {
   StellarError,
 } from '@/common/application/exceptions/stellar.error';
 import {
-  IAssetAmounts,
+  IAssetAmount,
   IStellarRepository,
   ISubmittedTransaction,
   STELLAR_REPOSITORY,
 } from '@/common/application/repository/stellar.repository.interface';
-import { ProductAssetService } from '@/modules/product-asset/application/services/product-asset.service';
 
 import {
   StellarTransaction,
@@ -31,32 +30,62 @@ export class StellarService implements OnModuleInit {
     private readonly stellarRepository: IStellarRepository,
     @Inject(STELLAR_TRANSACTION_REPOSITORY)
     private readonly stellarTransactionRepository: IStellarTransactionRepository,
-    private readonly stellarAssetService: ProductAssetService,
   ) {}
 
   async onModuleInit() {
     await this.stellarRepository.configureIssuerAccount();
-    await this.stellarRepository.createCoreAccounts();
-    await this.stellarAssetService.createAssetsForAllProducts();
   }
 
-  private validateStellarTransaction(
+  private createAssetCode(productId: number): string {
+    const PREFIX = 'ODOO';
+    const FILL_CHAR = '0';
+    const MAX_ASSET_CODE_LENGTH = 12;
+
+    const productCode = String(productId).padStart(
+      MAX_ASSET_CODE_LENGTH - PREFIX.length,
+      FILL_CHAR,
+    );
+
+    return PREFIX + productCode;
+  }
+
+  private transformOrderLinesToAssetAmounts(
+    orderLines: OrderLineDto[],
+  ): IAssetAmount[] {
+    const amounts: IAssetAmount[] = [];
+
+    for (const orderLine of orderLines) {
+      amounts.push({
+        assetCode: this.createAssetCode(orderLine.productId),
+        quantity: String(orderLine.quantity),
+      });
+    }
+
+    return amounts;
+  }
+
+  private validateTransaction(
     type: TRANSACTION_TYPE,
     transactions: StellarTransaction[],
   ): void {
     const validationMap = {
-      [TRANSACTION_TYPE.CONFIRM]: {
+      [TRANSACTION_TYPE.CREATE]: {
         length: 0,
-        error: ERROR_CODES.ORDER_ALREADY_CONFIRMED_ERROR,
+        error: ERROR_CODES.ORDER_UNABLE_TO_CREATE_ERROR,
+        prevType: undefined,
+      },
+      [TRANSACTION_TYPE.CONFIRM]: {
+        length: 1,
+        error: ERROR_CODES.ORDER_UNABLE_TO_CONFIRM_ERROR,
         prevType: undefined,
       },
       [TRANSACTION_TYPE.CONSOLIDATE]: {
-        length: 1,
+        length: 2,
         error: ERROR_CODES.ORDER_UNABLE_TO_CONSOLIDATE_ERROR,
         prevType: TRANSACTION_TYPE.CONFIRM,
       },
       [TRANSACTION_TYPE.DELIVER]: {
-        length: 2,
+        length: 3,
         error: ERROR_CODES.ORDER_UNABLE_TO_DELIVER_ERROR,
         prevType: TRANSACTION_TYPE.CONSOLIDATE,
       },
@@ -75,89 +104,45 @@ export class StellarService implements OnModuleInit {
     }
   }
 
-  private async executeStellarTransaction(
-    products: IAssetAmounts[],
-    type: TRANSACTION_TYPE,
-  ): Promise<ISubmittedTransaction> {
-    const operationMap = {
-      [TRANSACTION_TYPE.CONFIRM]: this.stellarRepository.confirmOrder,
-      [TRANSACTION_TYPE.CONSOLIDATE]: this.stellarRepository.consolidateOrder,
-      [TRANSACTION_TYPE.DELIVER]: this.stellarRepository.deliverOrder,
-    };
-
-    return await operationMap[type](products);
-  }
-
-  private async createStellarTransaction(
+  async executeTransaction(
     orderId: number,
     orderLines: OrderLineDto[],
     type: TRANSACTION_TYPE,
   ): Promise<string> {
-    const transactions =
-      await this.stellarTransactionRepository.getTransactionsForOrder(orderId);
-
-    this.validateStellarTransaction(type, transactions);
-
-    const products =
-      await this.stellarAssetService.transformOrderLinesToAssetAmounts(
-        orderLines,
-      );
-
-    const { hash, created_at } = await this.executeStellarTransaction(
-      products,
-      type,
-    );
-
-    await this.stellarTransactionRepository.create({
-      orderId,
-      hash,
-      timestamp: created_at,
-      type,
-    });
-
-    return hash;
-  }
-
-  async confirmOrder(
-    orderId: number,
-    orderLines: OrderLineDto[],
-  ): Promise<string> {
     try {
-      return await this.createStellarTransaction(
-        orderId,
-        orderLines,
-        TRANSACTION_TYPE.CONFIRM,
-      );
-    } catch (error) {
-      this.errorMapper.map(error);
-    }
-  }
+      const transactions =
+        await this.stellarTransactionRepository.getTransactionsForOrder(
+          orderId,
+        );
 
-  async consolidateOrder(
-    orderId: number,
-    orderLines: OrderLineDto[],
-  ): Promise<string> {
-    try {
-      return await this.createStellarTransaction(
-        orderId,
-        orderLines,
-        TRANSACTION_TYPE.CONSOLIDATE,
-      );
-    } catch (error) {
-      this.errorMapper.map(error);
-    }
-  }
+      this.validateTransaction(type, transactions);
 
-  async deliverOrder(
-    orderId: number,
-    orderLines: OrderLineDto[],
-  ): Promise<string> {
-    try {
-      return await this.createStellarTransaction(
+      const amounts = this.transformOrderLinesToAssetAmounts(orderLines);
+
+      let transaction: ISubmittedTransaction;
+      switch (type) {
+        case TRANSACTION_TYPE.CREATE:
+          transaction = await this.stellarRepository.createOrder(amounts);
+          break;
+        case TRANSACTION_TYPE.CONFIRM:
+          transaction = await this.stellarRepository.confirmOrder(amounts);
+          break;
+        case TRANSACTION_TYPE.CONSOLIDATE:
+          transaction = await this.stellarRepository.consolidateOrder(amounts);
+          break;
+        case TRANSACTION_TYPE.DELIVER:
+          transaction = await this.stellarRepository.deliverOrder(amounts);
+          break;
+      }
+
+      await this.stellarTransactionRepository.create({
         orderId,
-        orderLines,
-        TRANSACTION_TYPE.DELIVER,
-      );
+        type,
+        hash: transaction.hash,
+        timestamp: transaction.created_at,
+      });
+
+      return transaction.hash;
     } catch (error) {
       this.errorMapper.map(error);
     }
