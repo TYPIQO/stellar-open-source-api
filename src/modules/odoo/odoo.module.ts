8489402d86d2edecc16fd514aca960ced7d523fc
venutti <lucasvenutti23@gmail.com>
2024-03-19 08:48:54 -0300

    Add OdooAction domain & table

--- src/modules/odoo/odoo.module.ts
+++ src/modules/odoo/odoo.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { ODOO_ACTION_REPOSITORY } from './application/repository/odoo-action.repository.interface';
 import { OdooService } from './application/services/odoo.service';
+import { OdooActionSchema } from './infrastructure/persistence/odoo-action.schema';
+import { OdooActionTypeormRepository } from './infrastructure/persistence/odoo-action.typeorm.repository';
 
 @Module({
-  providers: [OdooService],
+  imports: [TypeOrmModule.forFeature([OdooActionSchema])],
+  providers: [
+    OdooService,
+    {
+      provide: ODOO_ACTION_REPOSITORY,
+      useClass: OdooActionTypeormRepository,
+    },
+  ],
   exports: [OdooService],
 })
 export class OdooModule {}
