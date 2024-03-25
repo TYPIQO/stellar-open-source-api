bea2d40e7994f829fa698837f99e3c77f6822d28
venutti <lucasvenutti23@gmail.com>
2024-03-25 14:47:14 -0300

    Update ACTIONS constant to add fields prop

--- src/modules/odoo/application/services/odoo.constants.ts
+++ src/modules/odoo/application/services/odoo.constants.ts
@@ -17,36 +17,52 @@ export enum STATE {
   DONE = 'done',
 }
 
+const FIELDS = {
+  [MODEL.SALE_ORDER]: {
+    model: MODEL.SALE_ORDER,
+    fields: ['id', 'order_line', 'state'],
+  },
+  [MODEL.STOCK_PICKING]: {
+    model: MODEL.STOCK_PICKING,
+    fields: ['id', 'sale_id', 'partner_id', 'state'],
+  },
+};
+
 export const ACTIONS: {
   [keyof in TRANSACTION_TYPE]: {
     serverAction: string;
     automation: string;
     endpoint: string;
-    state: string;
+    state: STATE;
+    fields: { model: MODEL; fields: string[] };
   };
 } = {
   create: {
     serverAction: 'CREATE ORDER ACTION',
     automation: 'CREATE ORDER AUTOMATION',
     endpoint: `${process.env.SERVER_URL}/api/stellar/create`,
-    state: 'draft',
+    state: STATE.DRAFT,
+    fields: FIELDS[MODEL.SALE_ORDER],
   },
   confirm: {
     serverAction: 'CONFIRM ORDER ACTION',
     automation: 'CONFIRM ORDER AUTOMATION',
     endpoint: `${process.env.SERVER_URL}/api/stellar/confirm`,
-    state: 'sale',
+    state: STATE.SALE,
+    fields: FIELDS[MODEL.SALE_ORDER],
   },
   consolidate: {
     serverAction: 'CONSOLIDATE ORDER ACTION',
     automation: 'CONSOLIDATE ORDER AUTOMATION',
     endpoint: `${process.env.SERVER_URL}/api/stellar/consolidate`,
-    state: 'assigned',
+    state: STATE.ASSIGNED,
+    fields: FIELDS[MODEL.STOCK_PICKING],
   },
   deliver: {
     serverAction: 'DELIVER ORDER ACTION',
     automation: 'DELIVER ORDER AUTOMATION',
     endpoint: `${process.env.SERVER_URL}/api/stellar/deliver`,
-    state: 'done',
+    state: STATE.DONE,
+    fields: FIELDS[MODEL.STOCK_PICKING],
   },
 };
