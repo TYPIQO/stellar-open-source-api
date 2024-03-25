dd064b87ef5843ca0447b4cc8de9c4a334a6645e
venutti <lucasvenutti23@gmail.com>
2024-03-25 14:49:12 -0300

    Refactor createCoreServerActions to create actions when they don't exist

--- src/modules/odoo/application/services/odoo.service.ts
+++ src/modules/odoo/application/services/odoo.service.ts
@@ -47,6 +47,26 @@ export class OdooService implements OnModuleInit {
     }
   }
 
+  private mergeSelections(
+    selections: string,
+    selectionIds: number[],
+  ): {
+    id: number;
+    name: string;
+  }[] {
+    const str = selections.replace(/'/g, '');
+    const regex = /\(([^)]+)\)/g;
+    const matches = [...str.matchAll(regex)];
+    const selectionNames = matches.map((match) =>
+      match[1].split(',')[0].trim(),
+    );
+
+    return selectionNames.map((name, index) => ({
+      name,
+      id: selectionIds[index],
+    }));
+  }
+
   async getOrderLinesForOrder(id: number): Promise<number[]> {
     const order = await this.odoo.searchRead<ISaleOrder>(
       MODEL.SALE_ORDER,
@@ -70,72 +90,6 @@ export class OdooService implements OnModuleInit {
     }));
   }
 
-  private mergeSelections(
-    selections: string,
-    selectionIds: number[],
-  ): {
-    id: number;
-    name: string;
-  }[] {
-    const str = selections.replace(/'/g, '');
-    const regex = /\(([^)]+)\)/g;
-    const matches = [...str.matchAll(regex)];
-    const selectionNames = matches.map((match) =>
-      match[1].split(',')[0].trim(),
-    );
-
-    return selectionNames.map((name, index) => ({
-      name,
-      id: selectionIds[index],
-    }));
-  }
-
-  private buildServerAction({
-    name,
-    modelId,
-    fieldIds,
-    endpoint,
-  }: {
-    name: string;
-    modelId: number;
-    fieldIds: number[];
-    endpoint: string;
-  }): IServerAction {
-    return {
-      name,
-      model_id: modelId,
-      binding_type: 'action',
-      state: 'webhook',
-      type: MODEL.SERVER_ACTION,
-      webhook_url: endpoint,
-      webhook_field_ids: fieldIds,
-    };
-  }
-
-  private buildAutomation({
-    name,
-    modelId,
-    serverActionId,
-    triggerFieldId,
-    selectionFieldId,
-  }: {
-    name: string;
-    modelId: number;
-    serverActionId: number;
-    triggerFieldId: number;
-    selectionFieldId: number;
-  }): IAutomation {
-    return {
-      name,
-      model_id: modelId,
-      active: true,
-      trigger: 'on_state_set',
-      action_server_ids: [serverActionId],
-      trigger_field_ids: [triggerFieldId],
-      trg_selection_field_id: selectionFieldId,
-    };
-  }
-
   private async getModelAndFields(
     modelName: MODEL,
     fieldNames: string[],
@@ -153,115 +107,67 @@ export class OdooService implements OnModuleInit {
     return { model: model[0], fields: fields };
   }
 
-  private async createOdooAction(
-    type: TRANSACTION_TYPE,
-    modelId: number,
-    fieldIds: number[],
-    stateFieldId: number,
-    stateSelections: { name: string; id: number }[],
-  ): Promise<void> {
+  private async createOdooAction(type: TRANSACTION_TYPE): Promise<void> {
+    const action = ACTIONS[type];
+    const { model, fields } = await this.getModelAndFields(
+      action.fields.model,
+      action.fields.fields,
+    );
+
+    const fieldIds = fields.map((field) => field.id);
+    const stateField = fields.find((field) => field.name === 'state');
+    const stateSelections = this.mergeSelections(
+      stateField.selection as string,
+      stateField.selection_ids,
+    );
     const selectionId = stateSelections.find(
       (selection) => selection.name === ACTIONS[type].state,
     ).id;
 
+    const serverAction: IServerAction = {
+      name: ACTIONS[type].serverAction,
+      model_id: model.id,
+      binding_type: 'action',
+      state: 'webhook',
+      type: MODEL.SERVER_ACTION,
+      webhook_url: ACTIONS[type].endpoint,
+      webhook_field_ids: fieldIds,
+    };
+
     const serverActionId = await this.odoo.create(
       MODEL.SERVER_ACTION,
-      this.buildServerAction({
-        name: ACTIONS[type].serverAction,
-        modelId,
-        fieldIds,
-        endpoint: ACTIONS[type].endpoint,
-      }),
+      serverAction,
     );
 
-    const automationId = await this.odoo.create(
-      MODEL.AUTOMATION,
-      this.buildAutomation({
-        name: ACTIONS[type].automation,
-        modelId,
-        serverActionId,
-        triggerFieldId: stateFieldId,
-        selectionFieldId: selectionId,
-      }),
-    );
+    const automation: IAutomation = {
+      name: ACTIONS[type].automation,
+      model_id: model.id,
+      active: true,
+      trigger: 'on_state_set',
+      action_server_ids: [serverActionId],
+      trigger_field_ids: [stateField.id],
+      trg_selection_field_id: selectionId,
+    };
+
+    const automationId = await this.odoo.create(MODEL.AUTOMATION, automation);
 
     await this.odooActionRepository.create({
       type,
       serverActionName: ACTIONS[type].serverAction,
-      serverActionId: serverActionId,
+      serverActionId,
       automationName: ACTIONS[type].automation,
-      automationId: automationId,
+      automationId,
     });
-  }
-
-  private async createSaleOrderAutomations(): Promise<void> {
-    const SALE_ORDER_TRANSACTIONS = [
-      TRANSACTION_TYPE.CREATE,
-      TRANSACTION_TYPE.CONFIRM,
-    ];
-    const SALE_ORDER_FIELDS = ['id', 'order_line', 'state'];
-
-    const { model, fields } = await this.getModelAndFields(
-      MODEL.SALE_ORDER,
-      SALE_ORDER_FIELDS,
-    );
-
-    const fieldIds = fields.map((field) => field.id);
-
-    const stateField = fields.find((field) => field.name === 'state');
-    const stateSelections = this.mergeSelections(
-      stateField.selection as string,
-      stateField.selection_ids,
-    );
-
-    for (const transaction of SALE_ORDER_TRANSACTIONS) {
-      await this.createOdooAction(
-        transaction,
-        model.id,
-        fieldIds,
-        stateField.id,
-        stateSelections,
-      );
-    }
-  }
-
-  private async createStockPickingAutomations(): Promise<void> {
-    const STOCK_PICKING_TRANSACTIONS = [
-      TRANSACTION_TYPE.CONSOLIDATE,
-      TRANSACTION_TYPE.DELIVER,
-    ];
-    const STOCK_PICKING_FIELDS = ['id', 'sale_id', 'partner_id', 'state'];
-
-    const { model, fields } = await this.getModelAndFields(
-      MODEL.STOCK_PICKING,
-      STOCK_PICKING_FIELDS,
-    );
-
-    const fieldIds = fields.map((field) => field.id);
-
-    const stateField = fields.find((field) => field.name === 'state');
-    const stateSelections = this.mergeSelections(
-      stateField.selection as string,
-      stateField.selection_ids,
-    );
-
-    for (const transaction of STOCK_PICKING_TRANSACTIONS) {
-      await this.createOdooAction(
-        transaction,
-        model.id,
-        fieldIds,
-        stateField.id,
-        stateSelections,
-      );
-    }
   }
 
   async createCoreServerActions(): Promise<void> {
     const actions = await this.odooActionRepository.getAll();
-
-    if (actions.length === 0) {
-      await this.createSaleOrderAutomations();
-      await this.createStockPickingAutomations();
+    const expectedActions = Object.values(TRANSACTION_TYPE);
+    const missingActions = expectedActions.filter(
+      (action) => !actions.find((a) => a.type === action),
+    );
+    for (const action of missingActions) {
+      await this.createOdooAction(action);
     }
   }
 }
