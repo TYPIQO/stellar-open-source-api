aa7cb57534bd7715f02fa6614e3dc3c6283d3757
venutti <lucasvenutti23@gmail.com>
2024-08-13 09:25:53 -0300

    Fix CancelOrderDto

--- src/modules/odoo/application/dto/cancel-order.dto.ts
+++ src/modules/odoo/application/dto/cancel-order.dto.ts
@@ -1,9 +1,11 @@
-import { Equals } from 'class-validator';
+import { Equals, IsInt } from 'class-validator';
 
 import { STATE } from '../services/odoo.state';
-import { SaleOrderDto } from './sale-order.dto';
 
-export class CancelOrderDto extends SaleOrderDto {
+export class CancelOrderDto {
+  @IsInt()
+  id: number;
+
   @Equals(STATE.CANCEL)
   state: string;
 }
